cc95767792fff4bc7121144aa08d66c14d3e5d69
Yash Gupta <yashbg7@gmail.com>
2021-07-07 15:04:14 +0530

    Add colors and font

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,26 @@ void main() => runApp(MaterialApp(
         appBar: AppBar(
           title: Text("Friends' Live Location Tracker"),
           centerTitle: true,
+          backgroundColor: Colors.red[600],
         ),
         body: Center(
-          child: Text('Welcome to the app!'),
+          child: Text(
+            'Welcome to the app!',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower',
+            ),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             print('Clicked');
           },
           child: Text('click'),
+          backgroundColor: Colors.red[600],
         ),
       ),
     ));
