b8ac0eba96b0aea39febc7b31da948abf8876bf4
Yash Gupta <yashbg7@gmail.com>
2021-07-07 14:11:18 +0530

    Create a basic Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,16 @@ void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Friends' Live Location Tracker"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('Welcome to the app!'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('Clicked');
+          },
+          child: Text('click'),
         ),
       ),
     ));
